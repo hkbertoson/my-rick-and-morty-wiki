6b42621ae6c46f832ecb9ca1665cab8e124c35f4
Hunter Bertoson <hunterkylebertoson@gmail.com>
2023-01-18 11:15:32 -0600

    Added adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Script from 'next/script';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+	<Script
+		id="Adsense-id"
+		data-ad-client="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9361134933535166"
+		async
+		strategy="afterInteractive"
+		onError={(e) => {
+			console.error('Script failed to load', e);
+		}}
+		src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+	/>;
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
